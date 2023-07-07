31817ab6a6d1a73b471e033e785b1e69e21efd64
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-07-06 23:05:01 -0400

    [Ruchita] Add: Create SimpleLinked List

---
+++ LinkedListPracticeProblem/LinkedListPracticeProblem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListPracticeProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1 create a simple Linked list 56-30-70
+            LinkedListNode list = new LinkedListNode();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            Console.ReadLine();
+        }
+    }
+}
